0238c878fe9f1812b5d8c5b38f885d16bb49d430
BasiaBmmH <barbaniella@gmail.com>
2023-02-13 18:33:17 +0100

    modul 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,14 +1,6 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-    public Calculator() {
-    }
-
-    public static void main(String[] args) {
-        add(7, 5);
-        subtract(21, 7);
-    }
-
 
     static int subtract(int num, int num1) {
         int result = num - num1;
--- kodilla-testing/src/test/java/com/kodilla/testing/calculator/TestingMain.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/calculator/TestingMain.java
@@ -2,13 +2,13 @@ package com.kodilla.testing.calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        int resultAdd = calc.add(7, 3);
-        int resultSubtract = calc.subtract(10, 7);
 
-        if(resultAdd == 10 && resultSubtract == 3){
+        int resultAdd = Calculator.add(7, 3);
+        int resultSubtract = Calculator.subtract(10, 7);
+
+        if (resultAdd == 10 && resultSubtract == 3) {
             System.out.println("Test: OK");
-        }else {
+        } else {
             System.out.println("Test: NOT-OK");
         }
 
