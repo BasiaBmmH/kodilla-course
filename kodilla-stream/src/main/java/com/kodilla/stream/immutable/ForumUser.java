16dc6b23a685c5d0aee3491d26ae30996f06ddaa
BasiaBmmH <barbaniella@gmail.com>
2023-04-11 21:07:50 +0200

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,8 +1,8 @@
 package com.kodilla.stream.immutable;
 
 public class ForumUser {
-   private final String username;
-   private final String realName;
+    private final String username;
+    private final String realName;
 
     public ForumUser(String username, String realName) {
         this.username = username;
