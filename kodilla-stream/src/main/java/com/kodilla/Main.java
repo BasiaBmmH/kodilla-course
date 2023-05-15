7abe5a105076ebc5408b01309fc26fe05dd28a74
BasiaBmmH <barbaniella@gmail.com>
2023-05-15 14:38:14 +0200

    7.6 introduction

--- kodilla-stream/src/main/java/com/kodilla/Main.java
+++ kodilla-stream/src/main/java/com/kodilla/Main.java
@@ -2,6 +2,14 @@ package com.kodilla;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr2 = {-7, -7, -7, 7, -7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, -7};
+
+        double sum = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            sum += arr2[i];
+        }
+
+        System.out.println(sum/ arr2.length);
+
     }
 }
